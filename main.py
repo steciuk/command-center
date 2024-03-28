c655bffdd3a306759edfd05b9dd953e969715804
Adam Steciuk <a.t.steciuk@gmail.com>
2024-03-28 21:23:49 +0100

    Subtitle downloading

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import os
 import time
 
+import cv2
+from napi import NapiPy
+
 
 def clear_screen():
     os.system("clear")
@@ -13,10 +16,8 @@ def schedule_shutdown():
     try:
         num_hours = int(num_hours)
         if num_hours < 0:
-            print("Podaj dodatnią liczbę godzin")
             return
     except ValueError:
-        print("Podaj poprawną liczbę godzin")
         return
 
     if num_hours == 0:
@@ -34,10 +35,12 @@ def plex_is_running():
 
 
 def start_plex():
+    clear_screen()
     os.system("sudo systemctl start plexmediaserver")
 
 
 def stop_plex():
+    clear_screen()
     os.system("sudo systemctl stop plexmediaserver")
 
 
@@ -52,6 +55,75 @@ def shutdown_scheduled_in_hours():
             return hh_mm
 
     return None
+
+
+def secods_to_hh_mm_ss(seconds):
+    return time.strftime("%H:%M:%S", time.gmtime(seconds))
+
+
+def display_movie_fps_and_duration(path):
+    cap = cv2.VideoCapture(path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    duration = frame_count / fps
+
+    duration = secods_to_hh_mm_ss(duration)
+
+    print(f"FPS: {fps:.2f}")
+    print(f"Czas trwania: {duration}")
+
+
+def download_subtitles():
+    clear_screen()
+    movie_path = input("Przeciągnij i upuść tutaj plik filmu: ")
+    movie_path = movie_path.strip("'")
+
+    if not os.path.exists(movie_path):
+        clear_screen()
+        print("Podany plik nie istnieje.")
+        input("Naciśnij Enter aby kontynuować...")
+        return
+
+    if os.path.isdir(movie_path):
+        clear_screen()
+        print("Podano folder zamiast pliku.")
+        input("Naciśnij Enter aby kontynuować...")
+        return
+
+    clear_screen()
+    try:
+        display_movie_fps_and_duration(movie_path)
+    except Exception as _:
+        clear_screen()
+        print("Podany plik najprawdopodobniej nie jest filmem.")
+        input("Naciśnij Enter aby kontynuować...")
+        return
+
+    hash = input("\nPodaj hash napisów: ")
+    hash = hash.strip()
+    if hash.startswith("napiprojekt:"):
+        hash = hash.split(":")[1]
+
+    if len(hash) != 32:
+        clear_screen()
+        print("Podano nieprawidłowy hash.")
+        print(f"+{hash}+")
+        input("Naciśnij Enter aby kontynuować...")
+        return
+
+    try:
+        napi = NapiPy()
+        _, _, subtitles = napi.download_subs(hash)
+        subs_path = napi.move_subs_to_movie(subtitles, movie_path)
+    except Exception as _:
+        clear_screen()
+        print("Nie udało się pobrać napisów.")
+        input("Naciśnij Enter aby kontynuować...")
+
+    clear_screen()
+    print(f"Pobrano napisy do: {subs_path}")
+    input("Naciśnij Enter aby kontynuować...")
 
 
 def main():
@@ -72,6 +144,8 @@ def main():
 
         options = []
 
+        options.append({"label": "Wyjdź", "action": exit})
+
         if shutdown_schedule_time:
             options.append(
                 {"label": "Anuluj wyłączenie komputera", "action": cancel_shutdown}
@@ -86,24 +160,21 @@ def main():
         else:
             options.append({"label": "Włącz Plex", "action": start_plex})
 
+        options.append({"label": "Pobierz napisy", "action": download_subtitles})
+
         for i, option in enumerate(options):
-            print(f"{i + 1}. {option['label']}")
+            print(f"{i}. {option['label']}")
 
-        print("q. Wyjdź\n")
-
-        last_selected = input("Wybierz opcję: ")
-
-        if last_selected == "q":
-            exit()
+        last_selected = input("\nWybierz opcję: ")
 
         try:
             last_selected = int(last_selected)
-            if last_selected < 1 or last_selected > len(options):
+            if last_selected < 0 or last_selected >= len(options):
                 continue
         except ValueError:
             continue
 
-        options[last_selected - 1]["action"]()
+        options[last_selected]["action"]()
 
 
 if __name__ == "__main__":
