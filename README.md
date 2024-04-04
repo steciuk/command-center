7e3be8479cf6ad9c25ab74ca62ee8894bb3fbec1
steciuk <a.t.steciuk@gmail.com>
2024-04-04 13:57:33 +0200

    Add napi instructions and requirements

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# tata-script
+# Centrum dowodzenia
+
+## Install
+
+```
+sudo apt install python3-dev
+```
+```
+python3 -m pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 import cv2
 from napi import NapiPy
+import pwd
 
 
 def clear_screen():
@@ -29,10 +30,11 @@ def schedule_shutdown():
 def cancel_shutdown():
     os.system("shutdown -c")
 
+def is_plex_present():
+  return os.system("systemctl list-unit-files | grep plexmediaserver.service") == 0
 
-def plex_is_running():
-    return os.system("systemctl is-active --quiet plexmediaserver")
-
+def is_plex_running():
+    return os.system("systemctl is-active --quiet plexmediaserver") == 0
 
 def start_plex():
     clear_screen()
@@ -70,24 +72,27 @@ def display_movie_fps_and_duration(path):
 
     duration = secods_to_hh_mm_ss(duration)
 
-    print(f"FPS: {fps:.2f}")
+    print(f"FPS: {fps:.3f}")
     print(f"Czas trwania: {duration}")
 
 
 def download_subtitles():
     clear_screen()
-    movie_path = input("Przeciągnij i upuść tutaj plik filmu: ")
+    print("1. Przeciągnij i upuść tutaj plik filmu")
+    print("2. Naciśnij Enter")
+    movie_path = input("\nPlik: ")
+    movie_path = movie_path.strip()
     movie_path = movie_path.strip("'")
 
     if not os.path.exists(movie_path):
         clear_screen()
-        print("Podany plik nie istnieje.")
+        print("Podany plik nie istnieje")
         input("Naciśnij Enter aby kontynuować...")
         return
 
     if os.path.isdir(movie_path):
         clear_screen()
-        print("Podano folder zamiast pliku.")
+        print("Podano folder zamiast pliku")
         input("Naciśnij Enter aby kontynuować...")
         return
 
@@ -96,19 +101,33 @@ def download_subtitles():
         display_movie_fps_and_duration(movie_path)
     except Exception as _:
         clear_screen()
-        print("Podany plik najprawdopodobniej nie jest filmem.")
+        print("Podany plik najprawdopodobniej nie jest filmem, bądź jest uszkodzony")
         input("Naciśnij Enter aby kontynuować...")
         return
 
-    hash = input("\nPodaj hash napisów: ")
+    print("\n1. Wejdź na stronę https://www.napiprojekt.pl/napisy-szukaj")
+    print("2. Wyszukaj film po tytule i wybierz pasujący wynik (https://imgur.com/a/yIAA3zG)")
+    print("  - Upewnij się, że tytuł i rok filmu są zgodne z tymi z pliku")
+    print("  - Jeśli nie ma wyników, spróbuj wyszukać po angielskim (lub polskim) tytule")
+    print("  - Czasem po wybraniu wyniku napiprojekt otwiera nową stronę z reklamą,")
+    print("    wtedy wróć do poprzedniej strony i ponów próbę wybrania wyniku")
+    print("3. Kliknij przycisk 'napisy' (https://imgur.com/a/k2fVPjo)")
+    print("4. Dopasuj napisy do czasu trwania i FPS filmu (https://imgur.com/a/GR0Hw1z)")
+    print("  - Najłatwiej posortować wyniki po czasie trwania,")
+    print("    klikając w nagłówek kolumny 'CZAS TRWANIA'")
+    print("  - Jeśli jest kilka pasujących wyników, wybierz ten z największą ilością pobrań")
+    print("5. Kliknij prawym przyciskiem myszy na nazwę pliku i wybierz 'Kopiuj odnośnik' (https://imgur.com/a/fPSGycC)")
+    print("6. Wklej skopiowany hash poniżej (CTRL+SHIFT+V) i naciśnij Enter")
+    print("  - Hash powinien wyglądać tak: 'napiprojekt:1234567890abcdef1234567890abcdef'")
+
+    hash = input("\nHash: ")
     hash = hash.strip()
     if hash.startswith("napiprojekt:"):
         hash = hash.split(":")[1]
 
     if len(hash) != 32:
         clear_screen()
-        print("Podano nieprawidłowy hash.")
-        print(f"+{hash}+")
+        print("Podano nieprawidłowy hash")
         input("Naciśnij Enter aby kontynuować...")
         return
 
@@ -118,28 +137,42 @@ def download_subtitles():
         subs_path = napi.move_subs_to_movie(subtitles, movie_path)
     except Exception as _:
         clear_screen()
-        print("Nie udało się pobrać napisów.")
+        print("Nie udało się pobrać napisów")
         input("Naciśnij Enter aby kontynuować...")
 
     clear_screen()
-    print(f"Pobrano napisy do: {subs_path}")
+    print("Poprawnie pobrano napisy!")
     input("Naciśnij Enter aby kontynuować...")
 
+
+def print_welcome_message():
+    usr = pwd.getpwuid(os.getuid())[0]
+
+    match usr:
+        case "tata":
+            print("Cześć kochany Tatusiu! :*")
+        case "ula":
+            print("Cześć kochana Mamusiu! :*")
+        case _:
+            print(f"Witaj {usr}!")
+            
 
 def main():
     last_selected = None
 
     while True:
         shutdown_schedule_time = shutdown_scheduled_in_hours()
-        plex_running = plex_is_running()
+        plex_present = is_plex_present()
+        plex_running = plex_present and is_plex_running()
 
         clear_screen()
-        print("Cześć kochany Tatusiu! :*")
+        print_welcome_message()
 
         print("===========================================")
         if shutdown_schedule_time:
             print(f"Wyłączenie komputera zaplanowane na {shutdown_schedule_time}")
-        print(f"Plex: {'ON' if plex_running == 0 else 'OFF'}")
+        if plex_present:
+          print(f"Plex: {'ON' if plex_running else 'OFF'}")
         print("===========================================")
 
         options = []
@@ -155,17 +188,18 @@ def main():
                 {"label": "Zaplanuj wyłączenie komputera", "action": schedule_shutdown}
             )
 
-        if plex_running == 0:
-            options.append({"label": "Wyłącz Plex", "action": stop_plex})
-        else:
-            options.append({"label": "Włącz Plex", "action": start_plex})
+        if plex_present:
+          if plex_running:
+              options.append({"label": "Wyłącz Plex", "action": stop_plex})
+          else:
+              options.append({"label": "Włącz Plex", "action": start_plex})
 
         options.append({"label": "Pobierz napisy", "action": download_subtitles})
 
         for i, option in enumerate(options):
             print(f"{i}. {option['label']}")
 
-        last_selected = input("\nWybierz opcję: ")
+        last_selected = input("\nWybierz opcję i naciśnij Enter: ")
 
         try:
             last_selected = int(last_selected)
